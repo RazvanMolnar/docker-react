dadccf8e31d8ad130e68d6f78a9296fdd60b2f56
Razvan Molnar <razvan.molnar@yahoo.com>
2021-02-09 22:39:04 +0200

    add text

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
-        <p>learn react Interesant</p>
+        <p>learn react feature</p>
       </header>
     </div>
   );
